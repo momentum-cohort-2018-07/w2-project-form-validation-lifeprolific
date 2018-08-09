05cb3ea385a6bc066636664cc7234dbc31059dc5
lifeprolific <thegregoman@gmail.com>
2018-08-09 16:07:11 -0400

    added recursive function to gather alerts

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var formElement = document.getElementById("parking-form")
 var inputFields = document.getElementsByTagName("input")
+var inputDivs = document.getElementsByClassName("input-field")
 var groupedIDs = ["car-year", "car-make", "car-model"]
 var alerts = {}
 
@@ -15,13 +16,28 @@ function submitClicked(event) {
         flagBlank(inputField)
     }
 
-
+    for (inputDiv of inputDivs) {
+        console.log(inputDiv.id + " alerts: " + gatherAlerts(inputDiv))
+    }
 }
 
 function flagBlank() {
     if (inputField.value.trim() === '') {
         alerts[inputField.id].push(inputField.id + " cannot be blank")
     }
+}
+
+function gatherAlerts(inputElement) {
+    var elementAlerts = []
+    for (htmlElement of inputElement.children) {
+        if (alerts[htmlElement.id] != undefined) {
+            elementAlerts = elementAlerts.concat(alerts[htmlElement.id])
+        }
+        else {
+            elementAlerts = elementAlerts.concat(gatherAlerts(htmlElement))
+        }
+    }
+    return elementAlerts
 }
 
 
