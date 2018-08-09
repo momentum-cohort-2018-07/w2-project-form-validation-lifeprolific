32781c9ee5e400d3b2d82d4d69688245f6337a50
lifeprolific <thegregoman@gmail.com>
2018-08-09 14:26:52 -0400

    added function to flag blank inputs

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 var formElement = document.getElementById("parking-form")
+var inputFields = document.getElementsByTagName("input")
+var specialIDs = ["car-year", "car-make", "car-model"]
 
 function submitClicked(event) {
     event.preventDefault()
-    console.log("submit click detected")
+    flagBlanks()
+}
+
+function flagBlanks() {
+    for (inputField of inputFields) {
+        if (inputField.value.trim() === '') {
+            if (specialIDs.includes(inputField.id)) {
+                inputField.parentElement.parentElement.classList.add("input-invalid")
+            }
+            else {
+                inputField.parentElement.classList.add("input-invalid")
+            }
+        }
+    }
 }
 
 formElement.addEventListener("submit", submitClicked)
