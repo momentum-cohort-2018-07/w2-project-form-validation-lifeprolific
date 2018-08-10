24d98b107a0302eef2eebe8b223eb3daa34ce7d4
lifeprolific <thegregoman@gmail.com>
2018-08-10 10:14:24 -0400

    refactoring to eliminate dictionary objects

--- script.js
+++ script.js
@@ -1,51 +1,20 @@
 var formElement = document.getElementById("parking-form")
 var inputFields = document.getElementsByTagName("input")
 var inputDivs = document.getElementsByClassName("input-field")
-var groupedIDs = ["car-year", "car-make", "car-model"]
-var alerts = {}
 
-// set up alerts object
-for (inputField of inputFields) {
-    alerts[inputField.id]=[]
+function findParentInputDiv (htmlElement) {
+    if (htmlElement.parentElement.classList[0] === "input-field") {
+        return htmlElement.parentElement
+    }
+    else {
+        return findParentInputDiv(htmlElement.parentElement)
+    }
 }
 
 function submitClicked(event) {
     event.preventDefault()
     for (inputField of inputFields) {
-        flagBlank(inputField)
-    }
-    for (inputDiv of inputDivs) {
-        updateAlerts(inputDiv)
-    }
-}
-
-function flagBlank() {
-    if (inputField.value.trim() === '') {
-        alerts[inputField.id].push(inputField.id + " cannot be blank")
-    }
-}
-
-function gatherAlerts(inputElement) {
-    var elementAlerts = []
-    for (htmlElement of inputElement.children) {
-        if (alerts[htmlElement.id] != undefined) {
-            elementAlerts = elementAlerts.concat(alerts[htmlElement.id])
-        }
-        else {
-            elementAlerts = elementAlerts.concat(gatherAlerts(htmlElement))
-        }
-    }
-    return elementAlerts
-}
-
-function updateAlerts(inputDiv) {
-    var elementAlerts = gatherAlerts(inputDiv)
-    var alertsDivExists = 1
-    if (elementAlerts.length > 0) {
-        var alertsDiv = document.createElement("div")
-        alertsDiv.classList.add("alerts")
-        alertsDiv.innerText = elementAlerts
-        inputDiv.appendChild(alertsDiv)
+        // validate field
     }
 }
 
