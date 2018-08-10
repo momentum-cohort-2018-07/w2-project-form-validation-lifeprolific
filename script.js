15d528f6837e60b00bed3eb321d417df510065a5
lifeprolific <thegregoman@gmail.com>
2018-08-10 13:52:48 -0400

    added validation, color alerts, message alerts

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 var formElement = document.getElementById("parking-form")
 var inputFields = document.getElementsByTagName("input")
 var inputDivs = document.getElementsByClassName("input-field")
+var fieldValidations = {
+    "name": [checkNotBlank],
+    "car-year": [checkNotBlank],
+    "car-make": [checkNotBlank],
+    "car-model": [checkNotBlank],
+    "start-date": [checkNotBlank],
+    "days": [checkNotBlank],
+    "credit-card": [checkNotBlank],
+    "cvv": [checkNotBlank],
+    "expiration": [checkNotBlank]
+}
 
-function findParentInputDiv (htmlElement) {
+function findParentInputDiv(htmlElement) {
     if (htmlElement.parentElement.classList[0] === "input-field") {
         return htmlElement.parentElement
     }
@@ -11,9 +22,51 @@ function findParentInputDiv (htmlElement) {
     }
 }
 
+function findError(htmlElement, errorClass) {
+    for (childElement of htmlElement.children) {
+        if (childElement.classList.contains(errorClass)) {
+            return childElement
+        }
+        else {
+            findError(childElement)
+        }
+    }
+    return false
+}
+
+function validate(inputElement) {
+    for (validationMethod of fieldValidations[inputElement.id]) {
+        var response = validationMethod(inputElement)
+        var errorClass = inputElement.id + "-" + response.errorType
+        var errorMessageDiv = findError(findParentInputDiv(inputElement), errorClass)
+        if (response.errorFound) {
+            if (!errorMessageDiv) {
+                var newErrorDiv = document.createElement("div")
+                newErrorDiv.classList.add(errorClass)
+                newErrorDiv.classList.add("error_message")
+                newErrorDiv.innerText = response.errorMessage
+                findParentInputDiv(inputField).appendChild(newErrorDiv)
+            }
+        }
+        else if (errorMessageDiv) {
+            errorMessageDiv.remove()
+        }
+    }
+    var parentInputDiv = findParentInputDiv(inputElement)
+    if(findError(parentInputDiv, "error_message")) {
+        parentInputDiv.classList.add("input-invalid")
+        parentInputDiv.classList.remove("input-valid")
+    }
+    else {
+        parentInputDiv.classList.add("input-valid")
+        parentInputDiv.classList.remove("input-invalid")
+    }
+}
+
 function checkNotBlank(inputElement) {
     var response = {
         errorFound: false,
+        errorType: "blank",
         errorMessage: ""
     }
     if (inputElement.value.trim() === "") {
@@ -26,9 +79,7 @@ function checkNotBlank(inputElement) {
 function submitClicked(event) {
     event.preventDefault()
     for (inputField of inputFields) {
-        if (checkNotBlank(inputField).errorFound) {
-            console.log(checkNotBlank(inputField).errorMessage)
-        }
+        validate(inputField)
     }
 }
 
